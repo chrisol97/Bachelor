e65ad8453a73ea7a2d3be7057b2d6a3cc76ae212
chrisol97 <166822669+chrisol97@users.noreply.github.com>
2025-03-10 20:56:39 +0100

    Add files via upload

--- spill.mjs
+++ spill.mjs
@@ -23,12 +23,23 @@ const Start = (maal) => {
 //#endregion
 
 // MUSIKK
-    let titlescreenmusic = new Audio("Lydfiler/Musikk/maybetitlescreenmusic.mp3");
+    let titlescreenmusic = new Audio("Lydfiler/Musikk/titlescreenmusic.mp3");
+
+// OBJEKTLYDER
+    let flyingplanesound = new Audio("Lydfiler/Objektlyder/planesound.mp3");
+
+// KARAKTERER
+
+    let mainCharVoice1 = new Audio("Lydfiler/Karakterer/mainCharVoice1.mp3");
 
 Start(startscreen);
 
 function startscreen() {
-    const startscreenobject = new Blocks.Image("Bilder/Karakterer/mainCharClosedMouth.png", {x: 265, y: 150, width: 500, height:500 });
+    const startscreenobject = new Blocks.Image("Bilder/Karakterer/mainCharClosedMouth.png", {x: 325, y: 150, width: 400, height:500 });
+    const skipbutton = new Blocks.Image("Bilder/Objekter/continueButton.png", {x: 200, y: 200, width: 200, height: 200});                           // HUSK Å FJERNE DETTE
+    Actions.Click(skipbutton, () => {
+        GaaTil(scene1);
+    })
     Actions.Click(startscreenobject, () => {
         GaaTil(titlescreen);
     })
@@ -37,11 +48,37 @@ function startscreen() {
 function titlescreen() {
     titlescreenmusic.play();
     const titlescreenbackground = new Blocks.Image("Bilder/Bakgrunner/kindergartenOutside.png", {x: -8, y: -8, width: 1088, height: 818});
-    const sign1 = new Blocks.Image("Bilder/Objekter/signHumlekollen.png", { x: 100, y: 350, width: 120, height: 150});
-    const sign2 = new Blocks.Image("Bilder/Objekter/signMarihønehula.png", { x: 475, y: 350, width: 75, height: 90});
-    const sign3 = new Blocks.Image("Bilder/Objekter/signMaurtua.png", { x: 940, y: 320, width: 100, height: 150});
-    const sun = new Blocks.Image("Bilder/Objekter/sun.png", { x: 900, y: -20, width: 200, height: 250});
-    const giraffetitlescreen = new Blocks.Image("Bilder/Karakterer/giraffeClosedMouth.png", { x: 200, y: 200, width: 300, height: 350});
-    const cattitlescreen = new Blocks.Image("Bilder/Karakterer/catClosedMouth.png", { x: 600, y: 290, width: 400, height: 600});
-    const mantitlescreen = new Blocks.Image("Bilder/Karakterer/manClosedMouth.png", { x: 770, y: 300, width: 75, height: 150});
+    const sign1 = new Blocks.Image("Bilder/Objekter/signHumlekollen.png", {x: 100, y: 350, width: 120, height: 150});
+    const sign2 = new Blocks.Image("Bilder/Objekter/signMarihønehula.png", {x: 475, y: 350, width: 75, height: 90});
+    const sign3 = new Blocks.Image("Bilder/Objekter/signMaurtua.png", {x: 940, y: 320, width: 100, height: 150});
+    const sun = new Blocks.Image("Bilder/Objekter/sun.png", {x: 900, y: -20, width: 200, height: 250});
+    const giraffetitlescreen = new Blocks.Image("Bilder/Karakterer/giraffeClosedMouth.png", {x: 300, y: 220, width: 150, height: 300});
+    const cattitlescreen = new Blocks.Image("Bilder/Karakterer/catClosedMouth.png", {x: 600, y: 490, width: 350, height: 300});
+    const mantitlescreen = new Blocks.Image("Bilder/Karakterer/manClosedMouth.png", {x: 780, y: 330, width: 50, height: 100});
+    const mainchartitlescreen = new Blocks.Image("Bilder/Karakterer/mainCharClosedMouth.png", {x: 200, y: 440, width: 200, height: 350});
+    titlescreenmusic.onended = () => {
+        GaaTil(scene1);
+    }
+}
+
+function scene1() {
+    const scene1background = new Blocks.Image("Bilder/Bakgrunner/kindergartenOutside.png", {x: -8, y: -8, width: 1088, height: 818});
+    const mainChar = new Blocks.Image("Bilder/Karakterer/mainCharClosedMouth.png", {x: 420, y: 800, width: 250, height: 400});
+    const flyingplane = new Blocks.Image("Bilder/Objekter/plane.png", { x: -250, y: 0, width: 220, height: 150});
+    const mainCharMoving = Actions.Tween(mainChar, 0, -6);
+    Utils.Bound(mainChar, Utils.Bounds(0, 400 , 1000, 5000), () => {
+        mainChar.hide();
+        mainCharMoving.pause();
+        Actions.Tween(flyingplane, 6.5, 0);
+        flyingplanesound.play();
+        new Blocks.CellAnimation(["Bilder/Karakterer/mainCharClosedMouth.png", "Bilder/Karakterer/mainCharClosedMouth.png", "Bilder/Karakterer/mainCharClosedMouth.png", "Bilder/Karakterer/mainCharOpenMouth.png"], {x: 420, y: 400, width: 250, height: 400, loop: true, auto: true});
+        mainCharVoice1.play();
+    })
+    mainCharVoice1.onended = () => {
+        GaaTil(scene2);
+    }
+}
+
+function scene2() {
+    
 }
