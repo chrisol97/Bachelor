732a97c5fd76113e2766cef65cc2d8f55f3fe8cc
chrisol97 <c_olsen1997@outlook.com>
2025-04-09 17:33:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -436,7 +436,7 @@ const cacheFiles = [
   "./Lydfiler/Objekterlyder/raceCar.mp3",
 ];
 
-self.AddEventListener("install", (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(async () => {
     const cache = self.open(cacheID);
     console.log("Adding alll files to cache")
