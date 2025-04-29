8c54209bd7b4ca625a5e23c9ee1148d34c829092
chrisol97 <c_olsen1997@outlook.com>
2025-04-29 11:43:33 +0200

    Update image.mjs

--- barneprat/blocks/image.mjs
+++ barneprat/blocks/image.mjs
@@ -1,7 +1,18 @@
 import * as Utils from "../utils/utils.mjs";
 import BaseBlock from "./baseBlock.mjs";
 
+// Dette er nytt - for aspect ratio
+const org_width = 500;
+const org_height = 400;
+
+const dW = (window.innerWidth / org_width);
+const dH = (window.innerHeight / org_height);
+console.log(dW, dH);
+
+// Ned hit
+
 class Image extends BaseBlock {
+
 
     constructor(source, settings = { x: 0, y: 0, width: 100, height: 100 }, container) {
         super();
@@ -11,6 +22,17 @@ class Image extends BaseBlock {
         this.y = settings.y || 0;
         this.width = settings.width || 100;
         this.height = settings.height || 100;
+
+
+        // Dette er nytt - for aspect ratio
+        this.width = Math.round(this.width * dW);
+        this.height = Math.round(this.height * dH);
+        settings.width = Math.round(settings.width * dW);
+        settings.height = Math.round(settings.height * dH);
+
+        // Ned hit
+
+
         this.img = document.createElement("img");
         this.img.src = this.source;
         Utils.positionElement(this.img, settings);
